49171b7e7f8411c78e56233c9882e7b9f5ad3880
7hemant <hemantmourya127@gmail.com>
2023-05-18 23:22:46 +0530

    updating

--- games/Hangman.py
+++ games/Hangman.py
@@ -2,17 +2,15 @@ import random
 from words import words
 
 
-
-
 def get_valid_word(words):
-    word = random.choice(words) # randomly choose something from the list
+    word = random.choice(words)  # randomly choose something from the list
     while "-" in words or " " in words:
         word = random.choice(words)
 
     return word.upper()
 
-def hangman():    
+
+def hangman():
     word = get_valid_word(words)
-    word_letter =set(word)
+    word_letter = set(word)
     # not completed yet
-    
