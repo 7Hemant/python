85ce70ea16338c88912d8f00d3eab067804892a1
7hemant <hemantmourya127@gmail.com>
2023-09-06 23:35:37 +0530

    django

---
+++ django/django1/mydb.py
@@ -0,0 +1,14 @@
+import mysql.connector
+database = mysql.connector.connect(
+    host= 'localhost',
+    user="root",
+    passwd="root"
+)
+#prepare a cursor object
+cursorObject = database.cursor()
+
+#create a database 
+cursorObject.execute("CREATE DATABASE eldercodatabse")
+
+
+print("all done")
